64aa031610642c74ac40da86a5717c80fcf1bd68
Pablo Sánchez González <pablosgon2@gmail.com>
2026-05-04 21:13:28 +0200

    feat: linear regression model

---
+++ ML.Models/ML.Regression/Domain/HousePricing.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML.Data;
+
+namespace ML.Regression.Domain
+{
+    public class HousePricing
+    {
+        [LoadColumn(0)]
+        public float SquareFootage { get; set; }
+
+        [LoadColumn(1)]
+        public float NumBedrooms { get; set; }
+
+        [LoadColumn(2)]
+        public float NumBathrooms { get; set; }
+
+        [LoadColumn(3)]
+        public float YearBuilt { get; set; }
+
+        [LoadColumn(4)]
+        public float LotSize { get; set; }
+
+        [LoadColumn(5)]
+        public float GarageSize { get; set; }
+
+        [LoadColumn(6)]
+        public string NeighbourhoodQuality { get; set; } = string.Empty;
+
+        [LoadColumn(7)]
+        public float HousePrice { get; set; }
+    }
+}
---
+++ ML.Models/ML.Regression/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML;
+using ML.Regression.Domain;
+
+MLContext context = new MLContext();
+
+IDataView housePricing = context.Data.LoadFromTextFile<HousePricing>("./HousePrice.csv", separatorChar: ',', hasHeader: true);
+
+var split = context.Data.TrainTestSplit(housePricing, testFraction: 0.2);
+IDataView trainingData = split.TrainSet;
+IDataView testData = split.TestSet;
+
+IEstimator<ITransformer> pipeline = context.Transforms.Categorical.OneHotEncoding(
+    $"{nameof(HousePricing.NeighbourhoodQuality)}Encoded", nameof(HousePricing.NeighbourhoodQuality))
+    .Append(context.Transforms.Concatenate(
+        "Features",
+        nameof(HousePricing.SquareFootage),
+        nameof(HousePricing.NumBedrooms),
+        nameof(HousePricing.NumBathrooms),
+        nameof(HousePricing.LotSize),
+        nameof(HousePricing.GarageSize),
+        $"{nameof(HousePricing.NeighbourhoodQuality)}Encoded"))
+    .Append(context.Transforms.NormalizeMeanVariance("Features"))
+    .Append(context.Regression.Trainers.Sdca(nameof(HousePricing.HousePrice), "Features"));
+
+ITransformer model = pipeline.Fit(trainingData);
+
+IDataView predictions = model.Transform(testData);
+var metrics = context.Regression.Evaluate(predictions, nameof(HousePricing.HousePrice));
+
+Console.WriteLine($"RMSE: {metrics.RootMeanSquaredError}");
+Console.WriteLine($"MAE: {metrics.MeanAbsoluteError}");
+Console.WriteLine($"R^2: {metrics.RSquared}");
